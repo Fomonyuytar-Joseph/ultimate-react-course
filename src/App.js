ed74bd84fbbca1fac0c7e92e5de6f37fda230c92
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2024-02-09 03:46:26 +0100

    components

--- src/App.js
+++ src/App.js
@@ -51,12 +51,31 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Header/>
+
+      <Footer/>
     </div>
   );
+}
+
+
+const Header=()=>{
+  return <h1>Fast React Pizza Co.</h1>
+
+
+}
+
+function Menu(){
+  return <div>
+    <h2> Our Menu</h2>
+    <Pizza />
+    <Pizza />
+    <Pizza />
+  </div>
+
+}
+function Footer(){
+return <footer>{new Date().toLocaleTimeString()} We're currently open</footer>
 }
 
 
